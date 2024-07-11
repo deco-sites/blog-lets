c2873a389b23d782fc1012b710398125157faf42
Leticia <leticiabsousad@gmail.com>
2024-07-11 13:59:49 -0300

    debuging

--- apps/deco/records.ts
+++ apps/deco/records.ts
@@ -1,2 +1,7 @@
 export { default } from "apps/records/mod.ts";
 export * from "apps/records/mod.ts";
+
+export default function Records(p: Props) {
+    console.log("env", {env: p.authToken.get()})
+    return fn(p)
+}
